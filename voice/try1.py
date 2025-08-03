42a02e7d303852fe2650503774238e541a9563fe
sabundhal <sabundhal@gmail.com>
2025-08-03 21:26:02 +0300

    html0

--- voice/try1.py
+++
@@ -1,8 +0,0 @@
-from inference import TTS_RVC
-from playsound import playsound
-
-tts = TTS_RVC(rvc_path="src\\rvc", model_path="models\\DenVot.pth", input_directory="input\\")
-tts.set_voice("ru-RU-DmitryNeural")
-path = tts(text="Привет, мир!", pitch=6)
-
-playsound(path)
